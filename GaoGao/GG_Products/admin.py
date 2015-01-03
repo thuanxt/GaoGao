a71c2d9544893576103902fe4f8ce72a8069cd1b
thuanxt <thuanxt@outlook.com>
2015-01-04 02:15:41 +0700

    add GG_Products model to admin s

--- GaoGao/GG_Products/admin.py
+++ GaoGao/GG_Products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from GG_Products.models import Category, CategoryDetail, Product, Brand, Location, Color, ProductDetail
 
 # Register your models here.
+list_model = [Category, CategoryDetail, Product, Brand, Location, Color, ProductDetail]
+admin.site.register(list_model)
