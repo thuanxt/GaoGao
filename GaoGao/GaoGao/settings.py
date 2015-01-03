73a6c3cce1afd00cc4447208b5f323d0ed7b59e9
thuanxt <thuanxt@outlook.com>
2015-01-03 17:58:17 +0700

    config databases postgres

--- GaoGao/GaoGao/settings.py
+++ GaoGao/GaoGao/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'GaoGao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gaogao',
+        'USER': 'gaogao',
+        'PASSWORD': 'gaogao2912',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
