3fabe907e328f7f834d66f08d3203391f7cdf43e
thuanxt <thuanxt@outlook.com>
2015-01-03 19:39:02 +0700

    create model Category and CategoryDetail

--- GaoGao/GG_Products/models.py
+++ GaoGao/GG_Products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cateName = models.CharField(max_length=200)
+    
+class CategoryDetail(models.Model):
+    cateId = models.ForeignKey(Category)
+    cateDetailName = models.CharField(max_length=200)
